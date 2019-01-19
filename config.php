b3505c349d7b9c75379909357b23c39d121cca86
piotr-wrobel <piotr.wrobel.stanislaw@gmail.com>
2019-01-19 18:51:14 +0100

    wydzielenie danych wrazliwych do osobnego konfigu (ignorowanego w .gitignore)

--- config.php
+++ config.php
@@ -3,11 +3,19 @@
 if(!defined('SYSTEM_VERSION')) define('SYSTEM_VERSION','1.1.2');
 if(!defined('SYSTEM_NAME')) define('SYSTEM_NAME','doJazdy');
 
+if (! defined('SECRET_CONFIG') )		define('SECRET_CONFIG','/secret_config.php');
+
+if (is_readable(dirname(__FILE__).SECRET_CONFIG))
+{
+	require_once dirname(__FILE__).SECRET_CONFIG;
+}else
+{
+	$database['password']	= 'secret_password_to_database';
+	$pin							= '121212';
+}
+
 //Configuracja bazy danych
 $database['host']		= 'localhost';
 $database['name']		= 'doJazdy';
 $database['user']		= 'dojazdy';
-$database['password']	= '23sfdFS#%12234FFSVS';
-
-$pin					= '236543';
 ?>
